ea64d49470a6c828e50bdc6cf304c3d7e24773b3
Logavarshhni23 <logavarshhnis2007@gmail.com>
2025-12-17 21:52:01 +0530

    connected mongodb atlas with backend

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ async function connectDB() {
     try{
         await mongoose.connect(process.env.MONGO_URL);
         console.log("MongoDB connected ✅");
+        console.log("Connected DB:", mongoose.connection.name);
     }
     catch(err){
         console.log("MongoDB connection error ❌",err.message);
